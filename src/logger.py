757be08efe4381f78d940402e4583ecfb34913ff
sqdnoises <sqdnoises@gmail.com>
2022-10-05 04:03:56 +0530

    fixit lol

--- src/logger.py
+++ src/logger.py
@@ -25,38 +25,7 @@ class Logger:
         Returns:
             str: the string you specified
         """
-        with open(self.loggerpath, "a") as logfile:
-            logfile.write(text+"\n")
-        if p:
-            print(text)
-        return text
-
-class AsyncLogger:
-    """
-    Class that implements custom logging with async.
-    """
-
-    def __init__(self, path: str = "logs", utc: bool = False) -> None:
-        self.path = path
-        self.utc = utc
-        if self.utc:
-            self.loggerpath = os.path.join(self.path, str(datetime.utcnow())+".txt")
-        else:
-            self.loggerpath = os.path.join(self.path, str(datetime.now())+".txt")
-
-    async def log(self, text: str = "", p: bool = True) -> str:
-        """
-        Log a string of text.
-
-        Args:
-            text (str, optional): string. Defaults to "".
-            p (bool, optional): specify whether to print it or not. Defaults to True.
-
-        Returns:
-            str: the string you specified
-        """
-        async with open(self.loggerpath, "a") as logfile:
-            logfile.write(text+"\n")
+        open(self.loggerpath, "a").write(text+"\n")
         if p:
             print(text)
         return text
