a24ae81d790031c48915b2f9ac2023e56d9832d3
sqdnoises <sqdnoises@gmail.com>
2022-10-06 11:32:20 +0530

    switch to GNU General Public License

--- src/database.py
+++ src/database.py
@@ -1,3 +1,20 @@
+"""
+ - Ahuri Leveller - Levelling Discord bot for Ahuri's Discord server. 
+ - Copyright (C) 2022 Arshdeep Singh
+ - 
+ - This program is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU General Public License as published by
+ - the Free Software Foundation; either version 3 of the License, or
+ - (at your option) any later version.
+ - 
+ - This program is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU General Public License for more details.
+ - 
+ - You should have received a copy of the GNU General Public License
+ - along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import os
 from utils import exc, blen, read, write
 from traceback import format_exception
